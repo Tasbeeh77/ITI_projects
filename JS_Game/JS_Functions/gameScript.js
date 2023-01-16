393901e8d1a41407aa5943ec5cabbe720acbb6ad
Tasbeeh Eissa <tasbeehgamal7@gmail.com>
2023-01-17 01:02:37 +0200

    adding modules

--- JS_Game/JS_Functions/gameScript.js
+++ JS_Game/JS_Functions/gameScript.js
@@ -1,40 +1,78 @@
-//Array of birds
+//shared variables
 const birdsArray = [
-    {
-        name: 'white',
-        gif: '../images/white.gif'
-    },
-    {
-        name: 'black',
-        gif: '../images/black.gif'
-    },
-    {
-        name: 'blue',
-        gif: '../images/blue.gif'
-    }
-]
+        {
+            name: 'white',
+            gif: '../images/white.gif'
+        },
+        {
+            name: 'black',
+            gif: '../images/black.gif'
+        },
+        {
+            name: 'blue',
+            gif: '../images/blue.gif'
+        }
+    ]
 let currentBirds = []
-let i = 0
+let countt = 0
 const birdSize = 250
 let time = 60
 let score = 0
 let killed = 0
-let isClicked = 0;
+let bombIsClicked = 0 //to remove bomb when it clicked and not let it continue moving
+let birdIsClicked = 0
+
 
 //create bird
 const createBirds = () => {
-    
-    let timerId;
-    let bird = document.createElement("embed")
-    let index = Math.floor(Math.random() * 3)
-    bird.src = birdsArray[index].gif
-    bird.classList.add("bird")
-    currentBirds.push(birdsArray[index].name)
-    console.log(currentBirds)
-    bird.style.top = Math.floor(Math.random() * (window.innerHeight - birdSize)) + 'px'
-    document.querySelector("body").append(bird)
-    moveRight(bird, 0, i)
-    i++
+        let birdsNumbers= Math.floor(Math.random()*10+1)
+
+            let bird = document.createElement("embed")
+            let index = Math.floor(Math.random() * 3)
+            bird.src = birdsArray[index].gif
+            bird.classList.add("bird")
+            currentBirds.push(birdsArray[index].name)
+            console.log(currentBirds)
+            bird.style.top = Math.floor(Math.random() * (window.innerHeight - birdSize)) + 'px'
+            document.querySelector("body").append(bird)
+            moveRight(bird, 0, countt,birdsArray)
+            countt++
+            bird.addEventListener("click", function () {
+                countScoreResultBird(bird.src.slice(29,34))
+                document.querySelector("h2[name=score]").innerHTML = `${score}`
+                killed += 1
+                document.querySelector("h2[name=kill]").innerHTML = `${killed}`
+                currentBirds.splice(countt- 1, 1)
+                birdIsClicked=1
+                countt--
+            })        
+
+}
+//bird moveRight
+const moveRight = (bird, left, index) => {
+    let timerId = setInterval(() => {
+        if (time > 0) {
+            if (left < (window.innerWidth - birdSize - 15) && birdIsClicked == 0) {
+                left += 15
+                bird.style.left = left + 'px'
+            }
+            else {
+                setTimeout(() => {
+                    document.querySelector("body").removeChild(bird)
+                    currentBirds.splice(index - 1, 1)
+                    countt--
+                }, 50);
+                clearInterval(timerId)
+                setTimeout(() => {
+                    birdIsClicked = 0
+                    createBirds()
+                }, 1000);
+            }
+        }
+        else {
+            clearInterval(timerId)
+        }
+    }, 30);
 }
 
 //create Bomb
@@ -47,48 +85,21 @@ const createBomb = () => {
     fallDown(bomb, 64)
     bomb.addEventListener("click", function () {
         bomb.src = "../images/explosion.png"
-        countScoreResult()
+        countScoreResultBomb()
         document.querySelector("h2[name=score]").innerHTML = `${score}`
         killed += currentBirds.length
         document.querySelector("h2[name=kill]").innerHTML = `${killed}`
         console.log("deleted");
         currentBirds = []
-        isClicked = 1;
+        bombIsClicked = 1;
     })
-}
-
-//bird moveRight
-const moveRight = (bird, left, index) => {
-    let timerId = setInterval(() => {
-        if (time > 0) {
-            if (left < (window.innerWidth - birdSize - 15) && isClicked == 0) {
-                left += 15
-                bird.style.left = left + 'px'
-            }
-            else {
-                setTimeout(() => {
-                    document.querySelector("body").removeChild(bird)
-                    currentBirds.splice(index - 1, 1)
-                    i--
-                }, 50);
-                clearInterval(timerId)
-                setTimeout(() => {
-                    isClicked = 0
-                    createBirds()
-                }, 1000);
-            }
-        }
-        else {
-            clearInterval(timerId)
-        }
-    }, 30);
 }
 
 //bomb falldown
 const fallDown = (bomb, top) => {
     let timerId = setInterval(() => {
         if (time > 0) {
-            if (top < (window.innerHeight - bomb.height - 10) && isClicked == 0) {
+            if (top < (window.innerHeight - bomb.height - 10) && bombIsClicked == 0) {
                 top += 10
                 bomb.style.top = top + 'px'
             }
@@ -98,9 +109,9 @@ const fallDown = (bomb, top) => {
                 }, 50);
                 clearInterval(timerId)
                 setTimeout(() => {
-                    isClicked = 0
+                    bombIsClicked = 0
                     createBomb()
-                }, 1000);
+                }, 7000);
             }
         }
         else {
@@ -109,8 +120,24 @@ const fallDown = (bomb, top) => {
     }, 60);
 }
 
+
+//display result
+const displayResult=result=>
+{
+    if (score > 50) {
+        document.querySelector("div h1[name=result]").innerText = "You Won!"
+        document.querySelector("div img[name=resultImg]").src = "../images/win.png"
+        result.classList.remove("hidden")
+    }
+    else {
+        document.querySelector("div h1[name=result]").innerText = "You Lose!"
+        document.querySelector("div img[name=resultImg]").src = "../images/lose.png"
+        result.classList.remove("hidden")
+    }
+}
+
 //countDown Timer
-let countDown = (timeObject,result) => {
+let countDown = (timeObject, result) => {
     let id = setInterval(() => {
         if (time > 0) {
             timeObject.innerText = `${--time}`
@@ -121,25 +148,14 @@ let countDown = (timeObject,result) => {
             sessionStorage.setItem("LastScore", score)
             document.querySelector("body").removeChild(document.querySelector(".bird"))
             document.querySelector("body").removeChild(document.querySelector(".bomb"))
-            if(score>50)
-            {
-                document.querySelector("div h1[name=result]").innerText="You Won!"
-                document.querySelector("div img[name=resultImg]").src="../images/win.png"
-                result.classList.remove("hidden")
-            }
-            else
-            {
-                document.querySelector("div h1[name=result]").innerText="You Lose!"
-                document.querySelector("div img[name=resultImg]").src="../images/lose.png"
-                result.classList.remove("hidden")  
-            }
+            displayResult(result)
         }
 
     }, 1000);
 }
 
 //count score
-const countScoreResult = () => {
+const countScoreResultBomb = (imag) => {
     for (let i = 0; i < currentBirds.length; i++) {
         if (currentBirds[i] == 'white') {
             score += 5
@@ -151,6 +167,17 @@ const countScoreResult = () => {
             score -= 10
         }
     }
+}
+const countScoreResultBird = (imag) => {
+        if (imag == 'white') {
+            score += 5
+        }
+        if (imag == 'black') {
+            score += 10
+        }
+        if (imag == 'blue.') {
+            score -= 10
+        } 
 }
 //Last user info
 const lastInfo = (LastVisit, LastScore) => {
@@ -179,6 +206,7 @@ window.addEventListener("load", function () {
     let result = document.querySelector("div[name=finalResult]")
     let timeObject = document.querySelector("h2[name=time]")
 
+
     //do
     nameObject.innerText = sessionStorage.getItem('name')
 
@@ -189,7 +217,7 @@ window.addEventListener("load", function () {
     //start Game
     startGame.onclick = () => {
         popUp.classList.remove("openPop")
-        countDown(timeObject,result)
+        countDown(timeObject, result)
         setTimeout(() => {
             createBomb()
         }, 1000);
@@ -205,5 +233,7 @@ window.addEventListener("load", function () {
     //cancel
     Cancel.onclick = () => {
         result.classList.add("hidden")
+        location.href = "http://127.0.0.1:5500/pages/index.html"
     }
 })
+
--- JS_Game/JS_Functions/indexScript.js
+++ JS_Game/JS_Functions/indexScript.js
@@ -1,11 +1,21 @@
+import { pascalCase , CheckName } from "../Modules/functions.js";
+   
     window.addEventListener("load",function(){
         //selectors
         let goButton =document.querySelector("button")
         let nameObject=document.querySelector("input")
+        let errorMessage=document.querySelector("span")
         //events
         goButton.onclick = function(){
-            sessionStorage.setItem("name", nameObject.value)
-            location.href = "http://127.0.0.1:5500/pages/game.html"
+            if(CheckName(nameObject.value)==true)
+            {
+                sessionStorage.setItem("name", pascalCase(nameObject.value))
+                location.href = "http://127.0.0.1:5500/pages/game.html"
+            }
+            else
+            {
+                errorMessage.classList.remove("hidden")
+            }
         }
 
     })
